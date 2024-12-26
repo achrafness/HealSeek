ba24d771efc449e4092fa7db854161313501fe55
abdou <a_sahki@estin.dz>
2024-12-26 22:28:03 +0100

    continue this landing page

--- client/app/_components/LandingPageContent.tsx
+++ client/app/_components/LandingPageContent.tsx
@@ -67,6 +67,36 @@ export default function LandingPageContent() {
                 <LandingCard />
                 <LandingCard />
             </div>
+            <div className="bg-white">
+
+                <div className='container mx-auto py-24 flex flex-col justify-around items-center'>
+                    <h1 className=' my-2 font-medium text-[22px] text-[#00BFA5]'>Book an appointment</h1>
+                    <div className='w-4/5 text-white mx-auto h-[360px] bg-gradient-to-r from-[#1678F2] to-[#65A8FB] flex flex-col justify-center items-center text-center px-14 rounded-[32px] '>
+                        <h1 className='text-[40px] font-semibold'>
+                            Schedule a virtual or presential appointment today
+                        </h1>
+                        <p className='text-[#F2F7FF] text-base font-normal'>
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Egestas egestas viverra turpis habitant eu sociis fermentum felis.
+                        </p>
+                        <button
+                            className='flex flex-row text-[#3A8EF6] bg-white text-xl justify-center items-center rounded-[100px] h-14 px-7 py-4 p-2 my-10 gap-2 w-fit '
+                            style={{
+                                boxShadow: '0px 8px 23px 0px #4184F73D'
+                            }}
+                        >
+                            <FiMessageCircle />
+                            Book an appointment
+
+                        </button>
+                    </div>
+                    <h1 className='my-6 font-medium text-[22px] text-[#00BFA5] mt-32'>
+                        why doctor Mathews
+                    </h1>
+                    <div className='w-4/5 mx-auto'>
+                        <Image src='/whyme.svg' alt='' width={517} height={679} className='w-full' />
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
