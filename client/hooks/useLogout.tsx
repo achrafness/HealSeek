890c9d550382af88dc5997957d2d2b1709d18f60
majd <m_zeghdar@estin.dz>
2025-01-24 23:05:15 +0100

    fixs

--- client/hooks/useLogout.tsx
+++ client/hooks/useLogout.tsx
@@ -5,18 +5,20 @@ import useAxiosPrivate from "./useAxiosPrivate";
 const useLogout = () => {
     const { setAuthState } = useAuthStore(state => state);
     const axiosPrivate = useAxiosPrivate();
+
     const logout = async () => {
-        setAuthState({
-            accessToken: '',
-            user: null,
-            role: ''
-        });
         try {
             const response = await axiosPrivate.get('/auth/logout');
             // localStorage.removeItem('token');
         } catch (error) {
             console.log(error);
         }
+
+        setAuthState({
+            accessToken: '',
+            user: null,
+            role: ''
+        });
 
     }
     return logout;
