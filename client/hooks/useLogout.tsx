363a0472e3e29b4b1c3172262a1284d47eb268f4
majd <m_zeghdar@estin.dz>
2025-01-24 23:00:15 +0100

    Fixing logout

--- client/hooks/useLogout.tsx
+++ client/hooks/useLogout.tsx
@@ -1,8 +1,10 @@
 import axios from "../api/axios";
 import { useAuthStore } from "../store/store";
+import useAxiosPrivate from "./useAxiosPrivate";
 
 const useLogout = () => {
     const { setAuthState } = useAuthStore(state => state);
+    const axiosPrivate = useAxiosPrivate();
     const logout = async () => {
         setAuthState({
             accessToken: '',
@@ -10,9 +12,7 @@ const useLogout = () => {
             role: ''
         });
         try {
-            const response = await axios.get('/auth/logout', {
-                withCredentials: true
-            });
+            const response = await axiosPrivate.get('/auth/logout');
             // localStorage.removeItem('token');
         } catch (error) {
             console.log(error);
