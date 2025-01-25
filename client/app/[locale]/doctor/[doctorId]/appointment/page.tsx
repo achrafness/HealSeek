63ee825f42007fb7d0c90d553b0ae8cc7d703ca8
majd <m_zeghdar@estin.dz>
2025-01-25 23:25:46 +0100

    adding some cors links

--- client/app/[locale]/doctor/[doctorId]/appointment/page.tsx
+++ client/app/[locale]/doctor/[doctorId]/appointment/page.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
-import PublicNavbar from '@/app/dashboard/patient/_components/PublicNavbar';
+import PublicNavbar from '@/app/[locale]/dashboard/patient/_components/PublicNavbar';
 import AppointmentContent from './_components/AppointmentContent';
-import axios from '@/api/axios';
 type Params = {
     params: {
         doctorId: string;
